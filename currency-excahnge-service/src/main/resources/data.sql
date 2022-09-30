058d27341ed20b011be3b54708b538509fac15b5
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2022-09-30 12:46:06 -0300

    Configure JPA and initialize data

---
+++ currency-excahnge-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into CURRENCY_EXCHANGE (id,currency_from,currency_to,conversion_multiple,environment) values (1001,'CAD','INR',60,'');
+insert into CURRENCY_EXCHANGE (id,currency_from,currency_to,conversion_multiple,environment) values (1002,'USD','INR',75,'');
+insert into CURRENCY_EXCHANGE (id,currency_from,currency_to,conversion_multiple,environment) values (1003,'AUD','INR',55,'');
+
