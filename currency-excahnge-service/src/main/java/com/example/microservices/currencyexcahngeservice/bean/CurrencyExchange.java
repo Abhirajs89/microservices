facf87803c58a700a9c6338b4a32a1a24ca6d900
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2022-09-30 12:13:41 -0300

    Added env.port in response dynamically

--- currency-excahnge-service/src/main/java/com/example/microservices/currencyexcahngeservice/bean/CurrencyExchange.java
+++ currency-excahnge-service/src/main/java/com/example/microservices/currencyexcahngeservice/bean/CurrencyExchange.java
@@ -9,6 +9,8 @@ public class CurrencyExchange {
     private String to;
     private BigDecimal conversionMultiple;
 
+    private String environment;
+
     public CurrencyExchange() {
     }
 
@@ -49,5 +51,13 @@ public class CurrencyExchange {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
     }
 }
