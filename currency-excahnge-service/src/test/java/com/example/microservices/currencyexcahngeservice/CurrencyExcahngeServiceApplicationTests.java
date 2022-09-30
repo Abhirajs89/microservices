8f1fecd75b26357612b9cc29e6f9c40a0a32e26f
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2022-09-30 10:56:06 -0300

    Added Currency Exchange project

---
+++ currency-excahnge-service/src/test/java/com/example/microservices/currencyexcahngeservice/CurrencyExcahngeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices.currencyexcahngeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExcahngeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
